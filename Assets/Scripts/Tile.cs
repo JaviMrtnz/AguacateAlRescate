80ba9057ed6345532eb2f8b65b9ab75a1f653614
al385783 <al385783@uji.es>
2022-12-11 18:04:37 +0100

    arreglo bug pathfinding

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -18,30 +18,33 @@ public class Tile : MonoBehaviour
     public float amount;
     private bool sizeIncrease;
 
-	private AudioSource source;
+    private AudioSource source;
+
+    public Node node;
 
     private void Start()
     {
-		source = GetComponent<AudioSource>();
+        source = GetComponent<AudioSource>();
         gm = FindObjectOfType<GM>();
         rend = GetComponent<SpriteRenderer>();
 
+        isWalkable = false;
+        node = Pathfinding.grid.NodeFromWorldPoint(transform.position);
     }
 
     public bool isClear() // does this tile have an obstacle on it. Yes or No?
     {
-        Collider2D col = Physics2D.OverlapCircle(transform.position, 0.2f, obstacles);
-        if (col == null)
-        {
-            return true;
-        }
-        else {
-            return false;
-        }
+        return node.walkable && !node.hasTree;
     }
-    
-    public void Highlight() {
-		
+
+    public bool isClearKnight()
+    {
+        return node.walkable;
+    }
+
+    public void Highlight()
+    {
+
         rend.color = highlightedColor;
         isWalkable = true;
     }
@@ -53,37 +56,53 @@ public class Tile : MonoBehaviour
         isCreatable = false;
     }
 
-    public void SetCreatable() {
+    public void SetCreatable()
+    {
         rend.color = creatableColor;
         isCreatable = true;
     }
 
     private void OnMouseDown()
     {
-        if (isWalkable == true) {
-            gm.selectedUnit.Move(this.transform);
-        } else if (isCreatable == true && gm.createdUnit != null) {
+        if (gm.playerTurn == 2) return;
+
+        if (isCreatable == true && gm.createdUnit != null)
+        {
             Unit unit = Instantiate(gm.createdUnit, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+            Pathfinding.grid.NodeFromWorldPoint(unit.transform.position).walkable = false;
+            Pathfinding.grid.NodeFromWorldPoint(unit.transform.position).hasUnit = true;
             unit.hasMoved = true;
             unit.hasAttacked = true;
+            unit.transform.parent = GameObject.FindGameObjectWithTag("unitParent").gameObject.transform;
             gm.ResetTiles();
             gm.createdUnit = null;
-        } else if (isCreatable == true && gm.createdVillage != null) {
-            Instantiate(gm.createdVillage, new Vector3(transform.position.x, transform.position.y, 0) , Quaternion.identity);
+        }
+
+        else if (isCreatable == true && gm.createdVillage != null)
+        {
+            Village village = Instantiate(gm.createdVillage, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+            village.transform.parent = GameObject.FindGameObjectWithTag("unitParent").gameObject.transform;
+            Pathfinding.grid.NodeFromWorldPoint(village.transform.position).walkable = false;
             gm.ResetTiles();
             gm.createdVillage = null;
+        }
+
+        else if (gm.selectedUnit != null && isWalkable && node.walkable)
+        {
+            gm.selectedUnit.Move(node);
         }
     }
 
 
     private void OnMouseEnter()
     {
-        if (isClear() == true) {
-			source.Play();
-			sizeIncrease = true;
+        if (isClear() == true)
+        {
+            source.Play();
+            sizeIncrease = true;
             transform.localScale += new Vector3(amount, amount, amount);
         }
-        
+
     }
 
     private void OnMouseExit()
@@ -94,7 +113,8 @@ public class Tile : MonoBehaviour
             transform.localScale -= new Vector3(amount, amount, amount);
         }
 
-        if (isClear() == false && sizeIncrease == true) {
+        if (isClear() == false && sizeIncrease == true)
+        {
             sizeIncrease = false;
             transform.localScale -= new Vector3(amount, amount, amount);
         }
